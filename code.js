1a4083e8a1d7eb5c185aaf844797882f838dd740
Mahery Gonin <mahery.gonin@gmail.com>
2024-10-01 10:12:17 +0200

    commit

--- code.js
+++ code.js
@@ -1,22 +1,40 @@
-let eleves = [
-  { name: "Alice", hasReviewed: false },
-  { name: "Bob", hasReviewed: false },
-  { name: "Charlie", hasReviewed: false },
-  { name: "David", hasReviewed: false },
-];
+/* 
 
-pickReviewer = (students) => {
-  let students_list = students.filter((stud) => !stud.hasReviewed);
-  let student = students_list[Math.floor(Math.random() * students_list.length)];
+ 1 - Écris une fonction sumOfEvenNumbers(numbers) qui prend en entrée un tableau d'entiers numbers et retourne
+  la somme de tous les nombres pairs présents dans le tableau.
+const numbers = [1, 2, 3, 4, 5, 6];
 
-  console.log("Désigné: " + student.name);
+*/
 
-  // Update List
-  let student_index = eleves.findIndex((item) => item.name == student.name);
-  eleves[student_index].hasReviewed = true;
+const numbers = [1, 2, 10, 20, 3, 4, 5, 6, 3, 6, -2, -3];
+
+const sumOfEvenNumbers = (numbers) => {
+  let sumArray = 0;
+  // for (let i in numbers) {
+  //   if (numbers[i] % 2 == 0) {
+  //     sumArray += numbers[i];
+  //   }
+  // }
+  numbers.map((num) => (num % 2 == 0 ? (sumArray += num) : ""));
+  return sumArray;
 };
 
-for (let i in eleves) {
-  pickReviewer(eleves);
-  windows.prompt("Press enter for next student", "null");
-}
+console.log(sumOfEvenNumbers(numbers));
+
+/* 
+
+2 - Écris une fonction maxNumber(numbers) qui prend en entrée un tableau d'entiers numbers et retourne le plus
+ grand nombre présent dans le tableau.
+const numbers2 = [12, 21, 67, 36, 49, 51];
+
+*/
+
+const numbers2 = [12, 21, 67, 36, 251, 49, 51, -20, -523];
+
+const maxNumber = (numbers) => {
+  return numbers.sort((a, b) => {
+    return b - a;
+  })[0];
+};
+
+console.log(maxNumber(numbers2));
