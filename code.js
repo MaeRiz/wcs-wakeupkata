337129fe4de5792fe086bb8718e7bc09854363cb
Mahery Gonin <mahery.gonin@gmail.com>
2024-10-01 09:26:44 +0200

    commit

--- code.js
+++ code.js
@@ -1,45 +1,22 @@
-// Very first Wake Up Kata la team :tada:
-// Le but va être d'écrire une fonction JavaScript que tu vas nommer filterByTitle, qui prend en paramètre un tableau d'objets movies et une chaîne de caractères searchedTitle .
-// Cette fonction doit retourner un nouveau tableau contenant uniquement les objets dont la propriété title contient la chaîne de caractères searchedTitle spécifiée en paramètre ET doit être insensible à la casse.
-
-const recentMovies = [
-  {
-    title: "The Great Adventure",
-    releaseDate: "2023-07-15",
-    actors: ["Alex Johnson", "Samantha Bloom", "Michael Cane"],
-  },
-  {
-    title: "Lost in Time",
-    releaseDate: "2023-05-22",
-    actors: ["Clara Hughes", "Robert Smith", "Emily Stone"],
-  },
-  {
-    title: "Mystery of the Blue Ocean",
-    releaseDate: "2023-08-30",
-    actors: ["Henry Gold", "Natalie Port", "Bruce Wayne"],
-  },
-  {
-    title: "Cybernetic",
-    releaseDate: "2023-09-12",
-    actors: ["Aaron Stark", "Lisa Ray", "Jeremy Irons"],
-  },
-  {
-    title: "The Hidden World",
-    releaseDate: "2023-10-05",
-    actors: ["Sophia Loren", "Daniel Craig", "Emma Watson"],
-  },
+let eleves = [
+  { name: "Alice", hasReviewed: false },
+  { name: "Bob", hasReviewed: false },
+  { name: "Charlie", hasReviewed: false },
+  { name: "David", hasReviewed: false },
 ];
 
-const filterByTitle = (movies, searchTitle) => {
-  let result = [];
-  for (let i in movies) {
-    if (movies[i].title.toUpperCase() == searchTitle.toUpperCase()) {
-      result.push(movies[i]);
-    }
-  }
-  return result.length > 0
-    ? result
-    : "Aucun film trouvé, essayez un autre titre";
+pickReviewer = (students) => {
+  let students_list = students.filter((stud) => !stud.hasReviewed);
+  let student = students_list[Math.floor(Math.random() * students_list.length)];
+
+  console.log("Désigné: " + student.name);
+
+  // Update List
+  let student_index = eleves.findIndex((item) => item.name == student.name);
+  eleves[student_index].hasReviewed = true;
 };
 
-console.log(filterByTitle(recentMovies, "the gReaT adventure"));
+for (let i in eleves) {
+  pickReviewer(eleves);
+  windows.prompt("Press enter for next student", "null");
+}
