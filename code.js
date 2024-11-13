ec305f07067bd1a8b51852b2af573488311ee425
Mahery Gonin <mahery.gonin@gmail.com>
2024-11-13 09:57:32 +0100

    commit

--- code.js
+++ code.js
@@ -1,40 +1,74 @@
-/* 
+// Tu travailles sur le site web d'un magasin en ligne qui liste des produits.
+// Chaque produit est noté par les utilisateurs et cette note influence l'ordre dans lequel les produits sont affichés.
+// Chaque produit est représenté par un objet contenant les informations suivantes : id, nom, catégorie, prix, et note moyenne.
+// A partir d'un tableau de ces produits, crée une fonction sortByRating() qui triera le tableau de la meilleure note à la moins élevée :
 
- 1 - Écris une fonction sumOfEvenNumbers(numbers) qui prend en entrée un tableau d'entiers numbers et retourne
-  la somme de tous les nombres pairs présents dans le tableau.
-const numbers = [1, 2, 3, 4, 5, 6];
+// Crée ensuite une fonction filterByCategory(categoryName) qui retournera la liste des produits de la catégorie indiquée en argument
 
-*/
+// DATA
+const products = [
+  {
+    id: 1,
+    name: "Smartphone XY",
+    category: "Électronique",
+    price: 799.99,
+    rating: 4.6,
+  },
+  {
+    id: 2,
+    name: "Basketball Pro Ball",
+    category: "Sport",
+    price: 29.99,
+    rating: 4.2,
+  },
+  {
+    id: 3,
+    name: "Casque Audio Sans Fil",
+    category: "Électronique",
+    price: 199.99,
+    rating: 4.8,
+  },
+  {
+    id: 4,
+    name: "Série de Romans Fantasy",
+    category: "Livres",
+    price: 59.99,
+    rating: 4.5,
+  },
+  {
+    id: 5,
+    name: "Montre Connectée",
+    category: "Électronique",
+    price: 249.99,
+    rating: 4.4,
+  },
+];
 
-const numbers = [1, 2, 10, 20, 3, 4, 5, 6, 3, 6, -2, -3];
-
-const sumOfEvenNumbers = (numbers) => {
-  let sumArray = 0;
-  // for (let i in numbers) {
-  //   if (numbers[i] % 2 == 0) {
-  //     sumArray += numbers[i];
-  //   }
-  // }
-  numbers.map((num) => (num % 2 == 0 ? (sumArray += num) : ""));
-  return sumArray;
+// FUNCTIONS
+const sortByRating = (products) => {
+  return products.sort(function (a, b) {
+    return b.rating - a.rating;
+  });
 };
 
-console.log(sumOfEvenNumbers(numbers));
-
-/* 
-
-2 - Écris une fonction maxNumber(numbers) qui prend en entrée un tableau d'entiers numbers et retourne le plus
- grand nombre présent dans le tableau.
-const numbers2 = [12, 21, 67, 36, 49, 51];
-
-*/
-
-const numbers2 = [12, 21, 67, 36, 251, 49, 51, -20, -523];
-
-const maxNumber = (numbers) => {
-  return numbers.sort((a, b) => {
-    return b - a;
-  })[0];
+const filterByCategory = (categoryName, products) => {
+  return products.filter(
+    (item) => item.category.toUpperCase() == categoryName.toUpperCase()
+  );
 };
 
-console.log(maxNumber(numbers2));
+// PRINTS FOR TESTS
+const PrintSortedByRating = () => {
+  console.log("Liste des produits par ordre de notes: ");
+  console.log(sortByRating(products));
+};
+
+const PrintFilterByCat = (cat) => {
+  console.log("Liste des produits par catégorie: " + cat);
+  console.log(filterByCategory(cat, products));
+};
+
+PrintSortedByRating();
+PrintFilterByCat("Livres");
+PrintFilterByCat("électronique");
+PrintFilterByCat("sporT");
